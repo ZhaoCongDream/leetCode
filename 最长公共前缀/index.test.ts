65388a64cd242dc1f48e96c4e8410a7ae6505947
zhaocong02 <zhaocong02@sunlands.com>
2022-03-31 17:05:47 +0800

    最长公共前缀

--- 最长公共前缀/index.test.ts
+++ 最长公共前缀/index.test.ts
@@ -1,6 +1,12 @@
 /*
  * @Date: 2022-03-31 16:16:21
  * @LastEditors: 赵聪
- * @LastEditTime: 2022-03-31 16:16:22
+ * @LastEditTime: 2022-03-31 17:04:30
  * @FilePath: /leetCode/最长公共前缀/index.test.ts
  */
+import { test, expect } from "@jest/globals";
+import { longestCommonPrefix } from "./index";
+test("test", () => {
+  expect(longestCommonPrefix(["flower", "flow", "flight"])).toBe('fl')
+  expect(longestCommonPrefix( ["dog","racecar","car"])).toBe('')
+});
--- 最长公共前缀/index.ts
+++ 最长公共前缀/index.ts
@@ -1,7 +1,21 @@
 /*
  * @Date: 2022-03-31 16:16:07
  * @LastEditors: 赵聪
- * @LastEditTime: 2022-03-31 16:17:52
+ * @LastEditTime: 2022-03-31 17:04:54
  * @FilePath: /leetCode/最长公共前缀/index.ts
  */
-export const longestCommonPrefix = () => {};
+
+export const longestCommonPrefix = (strList: string[]) => {
+  if (strList.length < 2) return strList[0];
+  let str = strList.shift();
+  for (let i = 0; i < strList.length && str.length > 0; i++) {
+    const element = strList[i];
+    for (let j = 0; j < str.length; j++) {
+      if (str.slice(0, j) !== element.slice(0, j)) {
+        str = str.slice(0, j - 1);
+        break;
+      }
+    }
+  }
+  return str;
+};
